3d37356433b1cd3e565a3b182d7a248c16d4090d
MrR0bo7 <neowalker93+github@gmail.com>
2016-12-10 00:54:56 +0100

    Add app icon UI design of new item activity

--- app/src/main/java/de/geek_hub/freezermanager/NewItemActivity.java
+++ app/src/main/java/de/geek_hub/freezermanager/NewItemActivity.java
@@ -1,12 +1,17 @@
 package de.geek_hub.freezermanager;
 
+import android.content.SyncStatusObserver;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class NewItemActivity extends AppCompatActivity {
 
@@ -14,6 +19,12 @@ public class NewItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item);
+
+        Spinner spinner = (Spinner) findViewById(R.id.categories);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+            R.array.categories, R.layout.categories_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
     @Override
